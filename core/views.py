c96b76dda1b0de5270f957191d7e34e4d0fa9b04
Faizan <faizan@faizan-hp-pavillion.14>
2016-02-28 22:07:13 +0530

    Created from view

--- core/views.py
+++ core/views.py
@@ -1,8 +1,15 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth import authenticate
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+from service.models import Service
+from customer.models import Customer
 
 
 def index(request):
@@ -11,8 +18,18 @@ def index(request):
 
 @login_required(login_url='/')
 def home(request):
-    return render(request, 'core/context.html')
-
+    current_month = datetime.now().month
+    context = RequestContext(request, {
+        "pending_service": Service.objects.filter(is_active=True,
+                                                  is_serviced=False),
+        "total_service": len(Service.objects.filter(is_active=True,
+                                                    is_serviced=True)),
+        "total_customer": len(Customer.objects.filter(is_active=True)),
+        "monthly_service": len(Service.objects.filter(service_date__month=current_month,
+                                                      is_active=True,
+                                                      is_serviced=True))})
+    return render_to_response('core/context.html',
+                              context_instance=context)
 
 def page_not_found(request):
     return render(request, 'core/page_not_found.html')
