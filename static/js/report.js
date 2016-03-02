b6e194abdc04284162d6e0cd6849424bb09d85dd
Faizan <faizan@faizan-hp-pavillion.14>
2016-03-03 00:38:36 +0530

    Updated autocomplate

--- static/js/report.js
+++ static/js/report.js
@@ -7,40 +7,62 @@ $(document).ready(function() {
     $("#from-date").datepicker();
 
     $("#generate-report").click(function(event) {
-      event.preventDefault();
-        
-        $.ajax({
-             type:"POST",
-             url:"",
-             data: $("#report-form").serialize(),
+        event.preventDefault();
 
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            },
-             success: function(data){
-             	$("#reportresult").html(data);
-                $("#generate-invoice").show();
-                $.toast({
-                    heading: 'Success',
-                    text: 'Report sucessfully generated !!!'+,
-                    icon: 'error',
-                    hideAfter: 4000,
-                    position: 'bottom-right'
-                })
-             },
-             error: function(){
-                $.toast({
-                    heading: 'Error',
-                    text: 'Something went wrong!!! Please try again',
-                    icon: 'error',
-                    hideAfter: 4000,
-                    position: 'bottom-right'
-                })
-             }
-        });
+        if ($("#from-date").val() && $("#till-date").val()){
+            $.ajax({
+                 type:"POST",
+                 url:"",
+                 data: $("#report-form").serialize(),
+
+                beforeSend: function(xhr) {
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                },
+                 success: function(data){
+                 	$("#reportresult").html(data);
+                    $("#generate-invoice").show();
+                    $.toast({
+                        heading: 'Success',
+                        text: 'Report Generated Sucessfully!!!',
+                        icon: 'error',
+                        hideAfter: 4000,
+                        position: 'bottom-right'
+                    })
+                 },
+                 error: function(){
+                    $.toast({
+                        heading: 'Error',
+                        text: 'Something went wrong!!! Please try again',
+                        icon: 'error',
+                        hideAfter: 4000,
+                        position: 'bottom-right'
+                    })
+                 }
+            });
+        }
+        else{
+            $.toast({
+                heading: 'Error',
+                text: 'Please fill out the Date fields!!!',
+                icon: 'error',
+                hideAfter: 4000,
+                position: 'bottom-right'
+            })
+        }
     });
 
     $("#generate-invoice").click(function(event) {
-        $("#report-form").submit();
+        if ($("#from-date").val() && $("#till-date").val()){
+            $("#report-form").submit();
+        }
+        else{
+            $.toast({
+                heading: 'Error',
+                text: 'Please fill out the Date fields!!!',
+                icon: 'error',
+                hideAfter: 4000,
+                position: 'bottom-right'
+            })            
+        }
     });
 });
